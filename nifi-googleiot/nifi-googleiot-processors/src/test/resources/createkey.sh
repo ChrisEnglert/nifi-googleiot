14a80d1e86bb5ad570523cf6ee6c05595f9eb1fb
chris.englert@live.com <chris.englert@live.com>
2019-03-06 22:11:21 +0100

    fix nar

--- nifi-googleiot/nifi-googleiot-processors/src/test/resources/createkey.sh
+++ nifi-googleiot/nifi-googleiot-processors/src/test/resources/createkey.sh
@@ -1,4 +1,4 @@
-openssl rsa -out private.key
+#!/bin/bash
 openssl genrsa -out rsa_private.pem 2048
 openssl rsa -in rsa_private.pem -pubout -out rsa_public.pem
 openssl pkcs8 -nocrypt -topk8 -inform PEM -in rsa_private.pem -outform DER -out rsa_private.der
